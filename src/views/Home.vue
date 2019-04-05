5e48a9a9fa6ffbc93eed8acd9aa12971490e36be
Dominik Korsa <dominik.korsa@gmail.com>
2019-04-05 11:31:20 +0200

    Fix line length error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,12 +44,18 @@ export default class Home extends Vue {}
       font-size: 32px;
       border-radius: 4px;
       text-align: center;
-      box-shadow: 0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12);
+      box-shadow:
+        0px 5px 5px -3px rgba(0, 0, 0, 0.2),
+        0px 8px 10px 1px rgba(0, 0, 0, 0.14),
+        0px 3px 14px 2px rgba(0, 0, 0, 0.12);
       transition: background-color 250ms, box-shadow 250ms;
 
       &:hover {
         background-color: #d84315;
-        box-shadow: 0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12);
+        box-shadow:
+          0px 11px 15px -7px rgba(0, 0, 0, 0.2),
+          0px 24px 38px 3px rgba(0, 0, 0, 0.14),
+          0px 9px 46px 8px rgba(0, 0, 0, 0.12);
       }
     }
   }
